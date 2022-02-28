5071da2254841a57f90155a5c618bb640ea2eb58
Ben_Argaman <94112749+benalon98@users.noreply.github.com>
2022-02-28 22:00:03 +0200

    adding files

---
+++ script.js
@@ -0,0 +1,46 @@
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("gallery-item")) {
+    const src = e.target.getAttribute("src");
+    document.querySelector(".modal-img").src = src;
+    const myModal = new bootstrap.Modal(
+      document.getElementById("gallery-modal")
+    );
+    myModal.show();
+  }
+});
+
+function validation() {
+  let form = document.getElementById("contact-form");
+  let email = document.getElementById("email").value;
+  let text = document.getElementById("text");
+  let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+  if (email.match(pattern)) {
+    form.classList.add("valid");
+    form.classList.remove("invalid");
+    text.innerHTML = "";
+  } else {
+    form.classList.remove("valid");
+    form.classList.add("invalid");
+    text.innerHTML = "Please Enter Valid Email Address";
+    text.style.color = "#ff0000";
+  }
+  if (email == "") {
+    form.classList.remove("valid");
+    form.classList.remove("invalid");
+    text.innerHTML = "";
+    text.style.color = "#00ff00";
+  }
+}
+function validateForm() {
+  let fname = document.getElementById("first-name").value;
+  let lname = document.getElementById("last-name").value;
+  let email = document.getElementById("email").value;
+
+  if (!fname || !lname || !email || form) {
+    alert("All inputs must be filled.");
+    return false;
+  }
+}
+
+// scroll animation
